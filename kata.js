fe13fbf2977cd4bb55b495e6e7680f5a5172e53c
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-02-18 09:12:34 +0000

    first commit of new kata

---
+++ kata.js
@@ -0,0 +1,7 @@
+// At School of Code, we have an Energiser activity around 3pm. Yesterday, we played table tennis... on the floor.
+// If there are 23 people on the course, everyone had a certain number of turns, and the total match time was totalTime seconds, write a function to show how long each person had with the paddle, to the next second, based on number of turns.
+
+//Solution
+function turnTime(turns, totalTime) {}
+
+module.exports = { turnTime };
